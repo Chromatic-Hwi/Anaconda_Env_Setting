34b4f2ccd267dba1505988cb8ed5a5cc88e02c56
Chromatic-Hwi <chromatic_365@naver.com>
2022-09-27 19:56:07 +0900

    220927 설치 버전 수정
      배치 파일에서 텐서플로우를 2.6.0으로 설치했었는데 텐서플로우가 최신 버전에선 gpu와 통합 설치될 뿐더러 이전의 2.6.0도 맞는 버전이 아니었기 때문에 수정 후 정상 작동 확인함.

--- Conda_install.bat
+++ Conda_install.bat
@@ -5,14 +5,14 @@ call %root%\Scripts\activate.bat %root%
 conda update --all -y
 pip install --upgrade pip -y
 
-pip install tensorflow==2.6.0
-pip install tensorflow-gpu==2.7.0
-pip install keras==2.6
+pip install keras
+pip install numpy
 pip install pydot
 pip install pydotplus
 pip install graphviz
 pip install opencv-python
 pip install jupyterthemes
+pip install tensorflow==2.7.0
 conda install pytorch==1.10.1 torchvision==0.11.2 torchaudio==0.10.1 -c pytorch -c conda-forge -y
 
 pause
